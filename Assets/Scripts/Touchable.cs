c672d6ab6decb472d77fea00baca27bc645a57e4
Daniel C <danclayson@gmail.com>
2016-03-12 13:46:46 -0800

    Added basic 'grab' object and 'touchable' script

---
+++ Assets/Scripts/Touchable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Touchable : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered");
+        if(other.gameObject.tag == "controller")
+        {
+            Debug.Log("Main Entered");
+            other.GetComponent<grab>().enteredObject = transform.gameObject;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "controller")
+        {
+            other.GetComponent<grab>().enteredObject = null;
+        }
+    }
+}
