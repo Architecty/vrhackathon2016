58706e97037ff0229e366f661992770b3f7074dc
Daniel C <danclayson@gmail.com>
2016-03-13 00:18:15 -0800

    Can now select ceiling, floor, and wall mounted objects

--- Assets/Scripts/grab.cs
+++ Assets/Scripts/grab.cs
@@ -4,6 +4,9 @@ using System.Collections;
 [RequireComponent(typeof(SteamVR_TrackedObject))]
 public class grab : MonoBehaviour
 {
+    public LayerMask wallMask;
+    public LayerMask furnitureMask;
+    public LayerMask ceilingMask;
     public GameObject selectedObject = null;
     public  GameObject enteredObject = null;
     SteamVR_TrackedObject trackedObj;
@@ -20,16 +23,26 @@ public class grab : MonoBehaviour
         var device = SteamVR_Controller.Input((int)trackedObj.index);
         if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
         {
-            if(enteredObject != null)
-            {
-                selectedObject = enteredObject;
-                GrabObject(selectedObject);
+            if(selectedObject == null) { 
+                if(enteredObject != null)
+                {
+                    GrabObject(enteredObject);
+                } else
+                {
+                    transform.FindChild("Laser").gameObject.SetActive(true);
+                }
             }
         }
         if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
         {
-            releaseObject(selectedObject);
-            selectedObject = null;
+            if (selectedObject) { 
+                releaseObject(selectedObject);
+                selectedObject = null;
+            } else if(transform.FindChild("Laser").gameObject.active == true)
+            {
+                transform.FindChild("Laser").gameObject.SetActive(false);
+                GrabObject(raycastFurniture());
+            }
         }
         if (selectedObject != null)
         {
@@ -40,16 +53,17 @@ public class grab : MonoBehaviour
 
     void GrabObject(GameObject whichObject)
     {
+        selectedObject = whichObject;
         switch (whichObject.tag)
         {
             case "furniture":
                 pickupFurniture(whichObject);
                 break;
             case "wallMounted":
-
+                pickupWallMounted(whichObject);
                 break;
             case "ceilingMounted":
-
+                pickupCeilingMounted(whichObject);
                 break;
         }
     }
@@ -63,10 +77,10 @@ public class grab : MonoBehaviour
 
                 break;
             case "wallMounted":
-
+                releaseWallMounted(whichObject);
                 break;
             case "ceilingMounted":
-
+                releaseCeilingMounted(whichObject);
                 break;
         }
     }
@@ -85,12 +99,130 @@ public class grab : MonoBehaviour
 
     void pickupWallMounted(GameObject whichObject)
     {
+        Destroy(whichObject.GetComponent<Rigidbody>());
         whichObject.transform.SetParent(transform);
     }
     void releaseWallMounted(GameObject whichObject)
     {
         whichObject.AddComponent<Rigidbody>();
+        whichObject.GetComponent<Rigidbody>().isKinematic = true;
+        whichObject.GetComponent<Rigidbody>().useGravity = false;
         whichObject.transform.SetParent(null);
-        whichObject.GetComponent<Rigidbody>().velocity = (transform.position - lastPosition) / Time.deltaTime;
+        Vector3 wallPoint = findWallPoint(whichObject);
+        Vector3 orientation = findWallNormal(whichObject);
+        if (wallPoint != Vector3.zero && orientation != Vector3.zero)
+        {
+            StartCoroutine(moveObject(whichObject, whichObject.transform.position, wallPoint, whichObject.transform.eulerAngles, new Vector3(0f, orientation.y, 0f), .5f));
+        }
+    }
+
+    void pickupCeilingMounted(GameObject whichObject)
+    {
+        Destroy(whichObject.GetComponent<Rigidbody>());
+        whichObject.transform.SetParent(transform);
+    }
+    void releaseCeilingMounted(GameObject whichObject)
+    {
+        whichObject.AddComponent<Rigidbody>();
+        whichObject.GetComponent<Rigidbody>().isKinematic = true;
+        whichObject.GetComponent<Rigidbody>().useGravity = false;
+        whichObject.transform.SetParent(null);
+        Vector3 wallPoint = findCeilingPoint(whichObject);
+        Vector3 orientation = findCeilingNormal(whichObject);
+        if (wallPoint != Vector3.zero && orientation != Vector3.zero)
+        {
+            StartCoroutine(moveObject(whichObject, whichObject.transform.position, wallPoint, whichObject.transform.eulerAngles, new Vector3(0f, orientation.y, 0f), .5f));
+        }
+    }
+
+    GameObject raycastFurniture()
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit, 100f, furnitureMask);
+        if (bHit)
+        {
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    Vector3 findWallNormal(GameObject whichObject)
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit, 100f, wallMask);
+        if (bHit)
+        {
+            return hit.normal + hit.collider.transform.eulerAngles;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    Vector3 findWallPoint(GameObject whichObject)
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit, 100f, wallMask);
+        if (bHit)
+        {
+            return hit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+    Vector3 findCeilingNormal(GameObject whichObject)
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit, 100f, ceilingMask);
+        if (bHit)
+        {
+            return hit.normal + hit.collider.transform.eulerAngles;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+
+    Vector3 findCeilingPoint(GameObject whichObject)
+    {
+        Ray raycast = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit, 100f, ceilingMask);
+        if (bHit)
+        {
+            return hit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+    IEnumerator moveObject(GameObject whichObject, Vector3 startPoint, Vector3 endPoint, Vector3 startRotation, Vector3 endRotation, float howLong)
+    {
+        Quaternion startRot = Quaternion.Euler(startRotation);
+        Quaternion endRot = Quaternion.Euler(endRotation);
+
+        float startTime = Time.time;
+        while(Time.time - startTime < howLong)
+        {
+            whichObject.transform.position = Vector3.Lerp(startPoint, endPoint, (Time.time - startTime) / howLong);
+            whichObject.transform.rotation = Quaternion.Lerp(startRot, endRot, (Time.time - startTime) / howLong);
+            yield return null;
+        }
+        whichObject.transform.position = endPoint;
+        whichObject.transform.eulerAngles = endRotation;
+
+
     }
 }
